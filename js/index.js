4a6986a891e1fd19c3ad04b7ac757794b63dc57a
ronronscelestes <julie.plantey@gmail.com>
2021-02-13 14:26:27 +0100

    homepage

--- js/index.js
+++ js/index.js
@@ -1,23 +1,86 @@
-// const catPupil = () => {
-//     let pupil = document.querySelector('.cat-pupil');
-//     let x = (pupil.getBoundingClientRect().left) + (pupil.clientWidth / 2);
-//     pupil.style.left = x;
-// }
+// 1 - Defining main container and its width
+const page = document.querySelector("body");
 
-// document.querySelector('body').addEventListener('mousemove', catPupil);
+// 2 - Defining eyes and their limits
+const eyes = document.querySelectorAll(".eye");
+let leftEye = {};
+let rightEye = {};
 
-let pupil = document.querySelector('.cat-pupil');
-let pupilContainer = document.querySelector('.cat-eye');
-
-window.addEventListener('mousemove', e => {
-    const mX = e.clientX;
-    const eyeLeftX = pupilContainer.getBoundingClientRect().x;
-    const eyeRightX = (pupilContainer.getBoundingClientRect().x + pupilContainer.getBoundingClientRect().width);
-    const pupilX = pupil.getBoundingClientRect().x;
-
-    if(pupilX > eyeLeftX && pupilX < eyeRightX) {
-        pupil.style.left = mX + 'px';
+eyes.forEach((eye) => {
+    if (eye.classList.contains("cat-eye")) {
+      leftEye = eye.getBoundingClientRect();
+    } else {
+      rightEye = eye.getBoundingClientRect();
     }
-    // console.log(pupilContainer.getBoundingClientRect());
-})  
+});
+
+// 3 - Defining pupils and its width
+const leftPupil = document.querySelector(".pupil-left");
+const rightPupil = document.querySelector(".pupil-right");
+const pupilRadius = document.querySelector(".cat-pupil").getBoundingClientRect().width / 2;
+
+// 4 - Functions to adjust eyes position
+const adjustEye = (mousePos, eye, pupil) => {
+  if (mousePos < eye.left) {
+    pupil.style.left = 0;
+    pupil.style.right = "initial";
+  } else if (mousePos > eye.right - pupilRadius) {
+    pupil.style.left = "initial";
+    pupil.style.right = 0;
+  } else {
+    pupil.style.left = `${(mousePos - eye.left) / 2}px`;
+  }
+};
+
+// 5 - Listening to the mouse moves, calling the function for each eye
+page.addEventListener("mousemove", (e) => {
+  const mousePos = e.pageX;
+
+  adjustEye(mousePos, leftEye, leftPupil);
+  adjustEye(mousePos, rightEye, rightPupil);
+});
+
+
+
+
+// CONFETTI ANIMATION 
+
+const catSnakeBtn = document.querySelector('.button-container');
+const catSnakeSvg = document.querySelector('.button-container #svg');
+
+const animItemSnake = bodymovin.loadAnimation({
+  wrapper: catSnakeSvg,
+  animType: 'svg',
+  loop: false,
+  autoplay: false,
+  path: 'https://assets9.lottiefiles.com/packages/lf20_t1qvgine.json'
+});
+
+catSnakeBtn.addEventListener('mouseenter', () => {
+  catSnakeSvg.classList.remove('hide');
+  animItemSnake.goToAndPlay(0,true);
+})
+
+const getEmBtn = document.querySelector('.button-container:nth-child(2)');
+const getEmSvg = document.querySelector('.button-container:nth-child(2) #svg');
+
+const animItemGetEm = bodymovin.loadAnimation({
+  wrapper: getEmSvg,
+  animType: 'svg',
+  loop: false,
+  autoplay: false,
+  path: 'https://assets9.lottiefiles.com/packages/lf20_t1qvgine.json'
+});
+
+getEmBtn.addEventListener('mouseenter', () => {
+  getEmSvg.classList.remove('hide');
+  animItemGetEm.goToAndPlay(0,true);
+})
+
+
+
+
+
+
+
 
